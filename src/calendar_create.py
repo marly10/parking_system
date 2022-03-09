572ddf67f04a77846a39dd0445bc2bd9e537dcba
marly10 <mckmarly@icloud.com>
2022-03-09 11:15:06 -0800

    working version of automation -send and creates calendar

---
+++ src/calendar_create.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+from email.policy import default
+from icalendar import Calendar, Event, vCalAddress, vText
+import pytz
+from datetime import datetime, date, timedelta
+import os
+from pathlib import Path
+import calendar
+
+#print(find_first_monday(2022, 3, 8)) def find_first_monday(year, month, day): 
+def day_selector( day_date, variable_name):
+    
+ for month in range(1, 13):
+        cal = calendar.monthcalendar(datetime.today().year, datetime.today().month)
+        first_week  = cal[0]
+        second_week = cal[1]
+        third_week  = cal[2]
+        forth_week  = cal[3]
+        
+        try:
+            #2nd monday and tuesday of the month
+            if day_date == "MONDAY2":
+                    variable_name = second_week[calendar.MONDAY]
+                    return variable_name
+                
+            elif day_date == "TUESDAY2":
+                    variable_name = second_week[calendar.TUESDAY]
+                    return variable_name
+                
+                #4th monday and tuesday of the month
+            elif day_date == "MONDAY4":
+                    variable_name = forth_week[calendar.MONDAY]
+                    return variable_name
+                
+            elif day_date == "TUESDAY4":
+                    variable_name = forth_week[calendar.TUESDAY]
+                    return variable_name
+            
+            else: 
+                print("Broken") 
+
+        except IndexError:
+            print('No date found')
+            
+        finally:
+            print('This is always executed') 
+
+#testing  
+#file1 = day_selector("MONDAY2", "monday_second")
+#file2 = day_selector("TUESDAY2", "tuesday_second")
+#file3 = day_selector("MONDAY4", "monday_forth")
+#file4 = day_selector("TUESDAY2", "tuesday_monday_forth")
+#print("2nd MONDAY: "+str(file1))
+#print("2nd TUESDAY: "+str(file2))
+#print("4th MONDAY: "+str(file3))
+#print("4th TUESDAY: "+str(file4))
+    
+def create_calendar(calendar_name, date):
+        
+    cal = Calendar()
+    cal.add('attendee', 'MAILTO:rickymarly7@gmail.com')
+
+    # need to type cast the year, month and day as variables
+    event = Event()
+    event.add('summary', 'Street sweeping - Move Car')
+    event.add('dtstart', datetime(datetime.today().year, datetime.today().month, date, 15, 8, 0, tzinfo=pytz.utc))
+    event.add('dtend', datetime(datetime.today().year, datetime.today().month, date, 15, 8, 0, tzinfo=pytz.utc))
+    event.add('dtstamp', datetime(datetime.today().year, datetime.today().month, date, 0, 10, 0, tzinfo=pytz.utc))
+
+    organizer = vCalAddress('MAILTO:buildawebsite1010@gmail.com')
+    organizer.params['cn'] = vText('Ricky Batman')
+    organizer.params['role'] = vText('Car Owner')
+    event['organizer'] = organizer
+    event['location'] = vText('San Diego, CA')
+
+    # Adding events to calendar
+    cal.add_component(event)
+    
+    folder_path = '/Users/rickymarly/Desktop/dockerTest/python_calendar'
+
+    
+    #makes calendar_folder
+    if not os.path.exists(folder_path+'/calendar_invite'):
+        os.makedirs(folder_path+'/calendar_invite')
+
+    directory = folder_path+"/calendar_invite"
+    print("ics file will be generated at ", directory)
+    f = open(os.path.join(directory, calendar_name+'.ics'), 'wb')
+    f.write(cal.to_ical())
+    f.close()
+
+#calling function that makes/gets the right dates
+m2 = day_selector("MONDAY2", "monday_second")
+t2 = day_selector("TUESDAY2", "tuesday_second")
+m4 = day_selector("MONDAY4", "monday_forth")
+t4 = day_selector("TUESDAY4", "tuesday_forth")
+
+#creates 4 calendar_invites
+create_calendar("2nd_monday", m2)
+create_calendar("2nd_tuesday", t2)
+create_calendar("4th_monday", m4)
+create_calendar("4th_tuesday", t4)
---
+++ src/reminder_email.py
@@ -0,0 +1,63 @@
+# Import modules
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+import os
+
+def attach_file_to_email(email_message, filename):
+    
+    with open(filename, "rb") as f:
+        file_attachment = MIMEApplication(f.read())
+    # Add header/name to the attachments    
+    file_attachment.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
+    # Attach the file to the message
+    email_message.attach(file_attachment)
+
+    
+def email_file_sender():
+    # Set up the email addresses and password.#Please replace below with your email address and password
+    email_from = 'buildawebsite1010@gmail.com'
+    password = 'npxjcjnqcxfmwnmo'
+    email_to = ['rickymarly7@gmail.com', 'tricianmckenzie@gmail.com']
+
+
+    # Create a MIMEMultipart class, and set up the From, To, Subject fields
+    email_message = MIMEMultipart()
+    email_message['From'] = email_from
+    email_message['To'] = ", ".join(email_to)
+    email_message['Subject'] = 'Street Sweeping [AUTOMATION]'
+
+
+    # Attach more (documents)
+    ##############################################################
+    dirname = '/Users/rickymarly/Desktop/dockerTest/python_calendar/calendar_invite/'
+    
+    # giving file extension
+    ext = ('ics')
+    
+    # iterating over all files
+    for files in os.listdir(dirname):
+        if files.endswith(ext):
+            attach_file_to_email(email_message, '../calendar_invite/'+str(files))
+            print(files)  # printing file name of desired extension
+        else:
+            print("not there")
+            continue
+    ##############################################################
+    
+    # Convert it as a string
+    email_string = email_message.as_string()
+
+    # Connect to the Gmail SMTP server and Send Email
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.ehlo()
+        server.login(email_from, password)
+        server.sendmail(email_from, email_to, email_string)
+        server.close()
+        
+email_file_sender()
